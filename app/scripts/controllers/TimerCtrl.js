e47ac3100f66856ca0625a6b23aeb0ccf6b50524
Thomas Conkwright <tmconkwright@gmail.com>
2017-12-13 08:20:18 -0500

    Working on setup

---
+++ app/scripts/controllers/TimerCtrl.js
@@ -0,0 +1,27 @@
+(function() {
+    function TimerCtrl() {
+        this.start = function() {
+            
+        };
+
+        this.stop = function() {
+            
+        };
+
+        this.break_5 = function() {
+
+        };
+
+        this.break_15 = function() {
+            
+        };
+        
+        this.break_30 = function() {
+                        
+        };
+    }
+
+    angular
+        .module('blocTime')
+        .controller('TimerCtrl', [TimerCtrl]);
+})();
