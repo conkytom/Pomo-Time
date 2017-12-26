d9c77c61307a0c7ceb331c94fccf418488dbffe5
Thomas Conkwright <tmconkwright@gmail.com>
2017-12-26 13:06:32 -0500

    Ding plays at end of timer

--- app/scripts/services/Countdown.js
+++ app/scripts/services/Countdown.js
@@ -7,7 +7,15 @@
         //Sets the default state of the app
         Countdown.state = "startWork";
         
+        //sets pomodoro count to 0
         Countdown.pomodoro = 0;
+
+        //loads the ding sound that's played at the end of a session
+        var ding = new buzz.sound( "./assets/sounds/ding", {
+            formats: ['mp3'],
+            preload: true
+          });
+
         //Start function designed to be used to invoke a variety of times
         //and states, examples of current usable states are given in TimerCtrl
         Countdown.start = function(time, state) {
@@ -20,7 +28,6 @@
             //numbers one second at a time
             Countdown.descend = $interval(function() {
                 if (Countdown.display == 0) { 
-                    Countdown.addPomodoro();
                     Countdown.end();
                 }
                 else
@@ -39,23 +46,33 @@
         //Changes the state for when the countdown reaches 0 and gives 
         //time it ended
         Countdown.end = function() {
+            //Ends the descent
             $interval.cancel(Countdown.descend);
+            //plays a sound
+            ding.play();
+            //Used to show the time that the last session ended
+            Countdown.endTime = moment().format('h:mm a');
+            //changes the state
             if (Countdown.state == "break")
                 Countdown.state = "startWork";
-            else
+            else {
+                //adds pomodoro if a work session has ended
+                Countdown.addPomodoro();
                 Countdown.state = "startBreak";
-            Countdown.endTime = now;
+            }
         };
 
-        var now = Date()
+        
+    
 
+        //Adds pomodoros, every 4 pomodoros allows for a 15/30min break
         Countdown.addPomodoro = function(){
             if (Countdown.pomodoro == 4)
                 Countdown.pomodoro = 0;
             else
                 Countdown.pomodoro ++;    
-        }
-        
+        };
+
 
         return Countdown;
     }
